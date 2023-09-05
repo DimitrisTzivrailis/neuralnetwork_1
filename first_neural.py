c68dc8131ef08c547dcb82efac6adf312ffda072
Dimitris Tzivrailis <dimitris4tziv@gmail.com>
2023-09-05 20:48:49 +0200

    new+giovani

--- first_neural.py
+++ first_neural.py
@@ -3,7 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 np.seterr(divide='ignore', invalid='ignore')
-
+#giovani
 def load_data(filename):
     '''
     takes as input the filename and
